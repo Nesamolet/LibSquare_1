e53911cdf21da17afa8148faa9c4fdb56a5dc6f4
Nesamolet <megamozg03@mail.ru>
2022-07-16 23:28:39 +0300

    Add project files.

---
+++ ClassLibrary1/LibSquare.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibSquare
+{
+
+  
+    public class Circle
+    {
+
+        public  static void InputValidation(double r)
+        {
+            if (r <= 0) throw new Exception("Радиус не может ,быть меньше или равен нулю!");
+        }
+
+
+        public static double GetArea(double r)
+        {
+
+            InputValidation(r);
+
+            return Math.PI * Math.Pow(r, 2);
+
+        }
+
+    }
+    public class Triangle
+    {
+
+
+        public static void InputValidation(double a, double b, double c)
+        {
+            if ((a <= 0) || (b <= 0) || (c <= 0)) throw new Exception("Ни одна из сторон треугольника не может быть меньше или равна нуля!");
+        }
+
+        public static double GetArea(double a, double b, double c)
+        {
+
+            if (Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2))
+                return a * b / 2;
+
+            double p = (a + b + c) / 2;
+            return Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+
+
+        }
+
+    }
+
+
+
+
+
+}
---
+++ UnitTestProject1/LibSquareUnitTest.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using LibSquare;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class LibSquareUnitTest
+    {
+        [TestMethod]
+        public void CircleInputValidation()
+        {
+
+            Assert.ThrowsException<Exception>(() => LibSquare.Circle.InputValidation(-3));
+            Assert.ThrowsException<Exception>(() => LibSquare.Circle.InputValidation(0));
+        }
+
+        [TestMethod]
+        public void CircleGetArea()
+        {
+
+            int r = 3;
+          
+            Assert.AreEqual(Math.PI * r * r, Circle.GetArea(r));
+        }
+
+
+        [TestMethod]
+        public void TriangleInputValidation()
+        {
+
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(-3,1,1));
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(1, -3, 1));
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(1, 1, -3));
+
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(0, 1, 1));
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(1, 0, 1));
+            Assert.ThrowsException<Exception>(() => LibSquare.Triangle.InputValidation(1, 1, 0));
+        }
+
+        [TestMethod]
+        public void TriangleGetArea()
+        {
+
+            int result = 6;
+            Assert.AreEqual(6, Triangle.GetArea(3,4,5));
+
+        }
+
+
+    }
+}
